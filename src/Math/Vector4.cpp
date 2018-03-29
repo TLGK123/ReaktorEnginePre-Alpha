2b32ef10a14fa18ae5217430441f9905780cc1ae
xiaomingfun <1075112523@qq.com>
2018-03-29 19:15:22 +0800

    添加数学库

---
+++ src/Math/Vector4.cpp
@@ -0,0 +1,50 @@
+
+//= INCLUDES =======
+#include "Vector4.h"
+#include "Vector3.h"
+#include "Matrix.h"
+//==================
+
+//= NAMESPACES =====
+using namespace std;
+//==================
+
+namespace TmingEngine
+{
+	const Vector4 Vector4::One(1.0f, 1.0f, 1.0f, 1.0f);
+	const Vector4 Vector4::Zero(0.0f, 0.0f, 0.0f, 0.0f);
+
+	Vector4::Vector4(const Vector3& value, float w)
+	{
+		this->x = value.x;
+		this->y = value.y;
+		this->z = value.z;
+		this->w = w;
+	}
+
+	Vector4::Vector4(const Vector3& value)
+	{
+		this->x = value.x;
+		this->y = value.y;
+		this->z = value.z;
+		this->w = 0.0f;
+	}
+
+	Vector4 Vector4::Transform(const Vector3& lhs, const Matrix& rhs)
+	{
+		Vector4 result;
+		result.x = (lhs.x * rhs.m00) + (lhs.y * rhs.m10) + (lhs.z * rhs.m20) + rhs.m30;
+		result.y = (lhs.x * rhs.m01) + (lhs.y * rhs.m11) + (lhs.z * rhs.m21) + rhs.m31;
+		result.z = (lhs.x * rhs.m02) + (lhs.y * rhs.m12) + (lhs.z * rhs.m22) + rhs.m32;
+		result.w = 1 / ((lhs.x * rhs.m03) + (lhs.y * rhs.m13) + (lhs.z * rhs.m23) + rhs.m33);
+
+		return result;
+	}
+
+	string Vector4::ToString() const
+	{
+		char tempBuffer[200];
+		sprintf_s(tempBuffer, "X:%f, Y:%f, Z:%f, W:%f", x, y, z, w);
+		return string(tempBuffer);
+	}
+}
---
+++ src/Math/Vector4.h
@@ -0,0 +1,69 @@
+
+#pragma once
+
+//= INCLUDES ==================
+#include "../Core/EngineDefs.h"
+#include <string>
+//=============================
+
+namespace TmingEngine
+{
+	class Vector3;
+	class Matrix;
+
+	class ENGINE_CLASS Vector4
+	{
+	public:
+		Vector4()
+		{
+			x = 0;
+			y = 0;
+			z = 0;
+			w = 0;
+		}
+
+		Vector4(float x, float y, float z, float w)
+		{
+			this->x = x;
+			this->y = y;
+			this->z = z;
+			this->w = w;
+		}
+
+		Vector4(float value)
+		{
+			this->x = value;
+			this->y = value;
+			this->z = value;
+			this->w = value;
+		}
+
+		Vector4(const Vector3& value, float w);
+		Vector4(const Vector3& value);
+
+		~Vector4(){}
+
+		//= COMPARISON ================================================
+		bool operator==(const Vector4& rhs) const
+		{
+			return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
+		}
+
+		bool operator!=(const Vector4& rhs) const
+		{
+			return !(*this == rhs);
+		}
+		//=============================================================
+
+		static Vector4 Transform(const Vector3& lhs, const Matrix& rhs);
+
+		std::string ToString() const;
+
+		float x, y, z, w;
+
+		const float* Data() const { return &x; }
+
+		static const Vector4 One;
+		static const Vector4 Zero;
+	};
+}
