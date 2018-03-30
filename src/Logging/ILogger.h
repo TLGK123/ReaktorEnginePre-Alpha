682151b99bde3bfad06c89a422dce78f096de345
xiaomingfun <1075112523@qq.com>
2018-03-30 15:24:45 +0800

    添加基类继承

--- src/Logging/ILogger.h
+++ src/Logging/ILogger.h
@@ -9,10 +9,20 @@
 
 namespace TmingEngine
 {
+	enum LogType
+	{
+		Error,
+	    Assert,
+		Warning,
+		Log,
+		Exception
+	};
+
 	class ENGINE_CLASS ILogger
 	{
 	public:
 		virtual ~ILogger() {}
 		virtual void Log(const std::string& log, int type) = 0;
+		virtual bool IsLogTypeAllowed() { return true; };
 	};
 }
