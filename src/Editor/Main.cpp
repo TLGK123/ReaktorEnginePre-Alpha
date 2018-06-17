b57033bdbd6dd0fa2505db0a70613dc7d860f817
lillteblue <1075112523@qq.com>
2018-06-17 18:58:59 +0800

    添加imgui的组件

--- src/Editor/Main.cpp
+++ src/Editor/Main.cpp
@@ -5,11 +5,33 @@
 //  Created by blue on 2018/6/16.
 //
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+#include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+
 #include "Main.h"
 #include <iostream>
-#include <imgui>
 using namespace std;
 int main ()
 {
     cout<<"Hello World"<<endl;
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
 }
