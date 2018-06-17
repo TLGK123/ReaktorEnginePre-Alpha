b0b6ad6ab3d542aa21d8ad3aeb4d95599e8a4197
lillteblue <1075112523@qq.com>
2018-06-18 00:41:57 +0800

    设置glfw的上下文 初始化glad

--- src/Editor/Main.cpp
+++ src/Editor/Main.cpp
@@ -32,10 +32,25 @@ int main ()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  //使用的是核心模式(Core-profile)
     
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-    cout<<"Hello OS X"<<endl;
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // uncomment this statement to fix compilation on OS X
 #endif
     
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);                                 //通知GLFW将我们窗口的上下文设置为当前线程的主上下文了
+    
+    
+    //---------------GLAD是用来管理OpenGL的函数指针的---------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
     
     
     return 0;
