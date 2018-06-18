bb914ca4e259d478028e280bc211752619a3b77a
lillteblue <1075112523@qq.com>
2018-06-18 18:45:41 +0800

    标准的一个三角形

--- src/Editor/Main.cpp
+++ src/Editor/Main.cpp
@@ -40,18 +40,23 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  //函数声明，当窗口大小调整时触发的函数
 void processInput(GLFWwindow *window);                                      //处理按键输入的函数
 
-//#if !Debug
-//#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) // 去掉黑色控制台窗口
-//#endif //  !Debug
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
-//---------------------Data-------------
-float vertrices[] ={
-    -0.5f,-0.5,0.0f,
-    0.5f,0.5f,0.0f,
-    0.0f,0.5f,0.0f
-};                                                                  //标准化的坐标，0，0 在正中心 其他位置都是百分比
+#if !Debug
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) // 去掉黑色控制台窗口
+#endif //  !Debug
 
-//---------------------Edn Data---------
 int main ()
 {
     cout<<"Hello World"<<endl;
@@ -66,49 +71,7 @@ int main ()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // uncomment this statement to fix compilation on OS X
 #endif
     
-    //--------- OpenGL --对象------
-    unsigned  int VBO;
-    glGenBuffers(1,&VBO);               //设置缓冲对象的ID
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);  //把新创建的vbo 缓存对象 绑定到 GL_ARRAY_BUFFER上
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertrices),vertrices,GL_STATIC_DRAW); //把顶点的数据信息复制到 VBO的内存空间中去
-    
-    /*
-     GL_STATIC_DRAW ：数据不会或几乎不会改变。  位置数据不会改变，每次渲染调用时都保持原样，所以它的使用类型最好是GL_STATIC_DRAW
-     GL_DYNAMIC_DRAW：数据会被改变很多。       一个缓冲中的数据将频繁被改变
-     GL_STREAM_DRAW ：数据每次绘制时都会改变。  这样就能确保显卡把数据放在能够高速写入的内存部分。
-     */
-    ///其实这个就是申请一片内存空间，然后向内存空间里面写数据
-    //-----------
-    
-    
-    //-----------开始加入GLSL---------
-    
-    char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);    //创建一个 GL_VERTEX_SHADER 类型的shader
-    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);   // 1是源代码的数量
-    glCompileShader(vertexShader);
-    int success;
-    char info[512];
-    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
-    
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader,512,NULL,info);
-        std::cout<<"Shader 编译出差："<<info<<endl;
-        
-    }
-    //------------GLSL---------end--------
-    
-    
-    
-    
+   
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -127,9 +90,89 @@ int main ()
     }
     
     glViewport(0, 0, 800, 600);                                         // 前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）
-    
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  //注册一个窗口变化的事件 ，也就会每当尺寸变化就调用的函数
     
+    
+    //-----------开始加入GLSL---------
+    
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);    //创建一个 GL_VERTEX_SHADER 类型的shader
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);   // 1是源代码的数量
+    glCompileShader(vertexShader);
+    int success;
+    char info[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+    
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader,512,NULL,info);
+        std::cout<<"Shader 编译出差："<<info<<endl;
+        
+    }
+    
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, info);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info << std::endl;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    
+    //------------GLSL---------end--------
+    
+    //---------------------Data-------------
+    float vertices[] ={
+        -0.5f,-0.5,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f
+    };                                                                  //标准化的坐标，0，0 在正中心 其他位置都是百分比
+    
+    //---------------------Edn Data---------
+    
+    
+    //--------- OpenGL --对象------
+    unsigned int VBO ,VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1,&VBO);               //设置缓冲对象的ID
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);  //把新创建的vbo 缓存对象 绑定到 GL_ARRAY_BUFFER上
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW); //把顶点的数据信息复制到 VBO的内存空间中去
+    
+    /*
+     GL_STATIC_DRAW ：数据不会或几乎不会改变。  位置数据不会改变，每次渲染调用时都保持原样，所以它的使用类型最好是GL_STATIC_DRAW
+     GL_DYNAMIC_DRAW：数据会被改变很多。       一个缓冲中的数据将频繁被改变
+     GL_STREAM_DRAW ：数据每次绘制时都会改变。  这样就能确保显卡把数据放在能够高速写入的内存部分。
+     */
+    ///其实这个就是申请一片内存空间，然后向内存空间里面写数据
+    //-----------
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
     
     while(!glfwWindowShouldClose(window))                               //开启一个渲染循环
     {
@@ -138,9 +181,16 @@ int main ()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
         glfwSwapBuffers(window);                                        //双缓冲交换 --更新画面
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
     glfwTerminate();                                                    //结束后释放资源
     return 0;
 }
