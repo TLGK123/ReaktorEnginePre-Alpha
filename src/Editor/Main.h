0956a7b9b9c50c398b271b8b730cb03cf084c5ed
WangYiMing <1075112523@qq.com>
2018-07-10 10:23:26 +0800

    调整代码结构 编译通过

--- src/Editor/Main.h
+++ src/Editor/Main.h
@@ -10,19 +10,4 @@
 #include"Screen.h"
 #include <stdio.h>
 
-#include <learnopengl/shader_m.h>
-#include <learnopengl/camera.h>
-#include <learnopengl/model.h>
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;										// camera
-Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));                                 //默认相机的参数
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
-
-// timing
-float deltaTime = 0.0f;                                                     //每帧执行需要加权平均一下
-float lastFrame = 0.0f;
-
 #endif /* Main_h */
--- src/Editor/Screen.h
+++ src/Editor/Screen.h
@@ -1,17 +1,24 @@
 #ifndef Screen_h
 #define Screen_h
 
-#endif // !Screen_h
-
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
 #include <iostream>
 
 #include "imgui.h"
 #include "imgui_impl_glfw_gl3.h"
+
+#if !Debug
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) // 去掉黑色控制台窗口
+#endif //  !Debug
+
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  //函数声明，当窗口大小调整时触发的函数
 void processInput(GLFWwindow *window);                                      //处理按键输入的函数
@@ -39,3 +46,5 @@ public:
 	void Init();
 private:
 };
+
+#endif // !Screen_h
