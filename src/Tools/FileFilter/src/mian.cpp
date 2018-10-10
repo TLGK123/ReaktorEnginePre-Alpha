5b1ee48a3395c29f57074444d37031872e8b7918
lillteblue <1075112523@qq.com>
2018-10-10 23:55:44 +0800

    文件夹 过滤读取文件

--- src/Tools/FileFilter/src/mian.cpp
+++ src/Tools/FileFilter/src/mian.cpp
@@ -5,10 +5,41 @@
 #include<sstream>
 #include <io.h>
 #include <stdlib.h>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
-map<string, string> commands;
-vector<string> filters;
+std::map<string, string> commands;
+std::vector<string> filters;
+std::vector<std::string> files;
+
+int writeFile(string file)
+{
+	ofstream examplefile(file);
+	if (examplefile.is_open())
+	{
+		examplefile << "This is a line.\n";
+		examplefile << "This is another line.\n";
+		examplefile.close();
+	}
+	return 0;
+}
+
+int readFile(string file)
+{
+	char buffer[256];
+	ifstream examplefile(file);
+	if (!examplefile.is_open())
+	{
+		cout << "Error opening file"; exit(1);
+	}
+	while (!examplefile.eof())
+	{
+		examplefile.getline(buffer, 100);
+		cout << buffer << endl;
+	}
+	return 0;
+}
 
 std::vector<std::string> split(const std::string& s, char delimiter)
 {
@@ -21,8 +52,6 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 	}
 	return tokens;
 }
-
-std::vector<std::string> files;
 
 void listFiles(const char * dir)
 {
@@ -55,14 +84,13 @@ void listFiles(const char * dir)
 		}
 		else
 		{
-			string s = string(dir) + string(findData.name);
+			string s = string(dir) + "\\" + string(findData.name);
 			auto ext = split(findData.name, '.');
 			//cout <<"."<< *(ext.end()-1) << endl;
-			
+
 			vector<string>::iterator it;
-			string value ="."+ *(ext.end() - 1);
+			string value = "." + *(ext.end() - 1);
 			it = find(filters.begin(), filters.end(), value);
-			
 			if (it != filters.end())
 			{
 				files.push_back(s);
@@ -90,6 +118,7 @@ int main(int argc, char const *argv[])
 	for (iter = files.begin(); iter != files.end(); iter++)
 	{
 		cout << *iter << endl;
+		readFile(*iter);
 	}
 	return 0;
 }
