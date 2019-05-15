a7942d992dcce7e0990c8b4e79b3627481c73cb9
WangYiMing <1075112523@qq.com>
2019-05-15 21:56:15 +0800

    成功实现Scene渲染和游戏Game渲染分离

--- src/Editor/Core/Game.cpp
+++ src/Editor/Core/Game.cpp
@@ -23,7 +23,13 @@ void TmingEngine::Game::Update()
         ImGui::End();
         return;
     }
+    ImTextureID my_tex_id = (void *)ImageId;
+    float my_tex_w = 512;
+    float my_tex_h = 280;
     
+    ImGui::Text("%.0fx%.0f", my_tex_w, my_tex_h);
+    //----------------------------------------------------------------- -1 -1  Image reversal
+    ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), ImVec2(0, 0), ImVec2(-1, -1), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));
     
     ImGui::Text("this  is Detial Panel ");
     
