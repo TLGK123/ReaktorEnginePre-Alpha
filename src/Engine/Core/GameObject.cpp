5a4d2e0bb9d60f6e0e56cd0e1a38a776cc069e9e
WangYiMing <1075112523@qq.com>
2019-05-20 19:25:38 +0800

    创建GameObject对象

---
+++ src/Engine/Core/GameObject.cpp
@@ -0,0 +1,22 @@
+//
+//  GameObject.cpp
+//  01_CaculateCode
+//
+//  Created by WangYiMing on 2019/5/20.
+//
+
+//= INCLUDES ==========
+#include "GameObject.hpp"
+
+//====================
+
+ namespace TmingEngine
+{
+    GameObject::GameObject()
+    {
+        
+    }
+    
+    
+    
+}
---
+++ src/Engine/Core/GameObject.hpp
@@ -0,0 +1,26 @@
+//
+//  GameObject.hpp
+//  01_CaculateCode
+//
+//  Created by WangYiMing on 2019/5/20.
+//
+
+#ifndef GameObject_hpp
+#define GameObject_hpp
+#include "EngineDefs.h"
+#include <stdio.h>
+
+namespace TmingEngine {
+    
+    class ENGINE_CLASS GameObject
+    {
+        
+        public :
+        GameObject();
+         
+    };
+    
+}
+
+
+#endif /* GameObject_hpp */
