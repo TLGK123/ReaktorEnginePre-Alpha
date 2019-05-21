76f4076dfa3067033105e37fe84b68f659d9ced1
blue <1075112523@qq.com>
2019-05-22 01:13:23 +0800

    可以选中层级结构

--- src/Editor/Core/Hierachy.cpp
+++ src/Editor/Core/Hierachy.cpp
@@ -16,11 +16,29 @@ namespace TmingEngine
         for (ite = root->m_children.begin();ite != root->m_children.end() ; ite++) {
             if ((*ite)->m_children.size()>0)
             {
-                if (ImGui::TreeNode((*ite)->GetGameObject()->name.c_str()))
+                ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
+                node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+                int tempIndex = 9 ,node_clicked;
+                bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)tempIndex, node_flags,(*ite)->GetGameObject()->name.c_str());
+                if (ImGui::IsItemClicked())
                 {
-                showTransform((*ite)->GetGameObject()->transform);
-                ImGui::TreePop();
+                    node_clicked =6;
+                    Debug::Log("选中了场景中的物体$$$$$$$$ %s \n",(*ite)->GetGameObject()->name.c_str());
+                    ImGui::TreePop();
                 }
+                if (node_open)
+                {
+                    showTransform((*ite)->GetGameObject()->transform);
+                 
+                }
+                
+                
+//                if (ImGui::TreeNode((*ite)->GetGameObject()->name.c_str()))
+//                {
+//                    showTransform((*ite)->GetGameObject()->transform);
+//                    ImGui::TreePop();
+//                }
+                
             }else
             {
                 ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
@@ -29,8 +47,8 @@ namespace TmingEngine
                 ImGui::TreeNodeEx((void*)(intptr_t)tempIndex, node_flags,(*ite)->GetGameObject()->name.c_str());
                 if (ImGui::IsItemClicked())
                 {
-                    
                     Debug::Log("选中了场景中的物体 %s \n",(*ite)->GetGameObject()->name.c_str());
+                    ImGui::TreePop();
                 }
             }
             
