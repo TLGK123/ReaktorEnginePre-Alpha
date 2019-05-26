a7c81953c75515e79847c5771ffab473646a6aac
blue <1075112523@qq.com>
2019-05-26 10:43:01 +0800

    树形选中哈哈

--- src/Editor/Core/Hierachy.cpp
+++ src/Editor/Core/Hierachy.cpp
@@ -19,21 +19,31 @@ namespace TmingEngine
             if ((*ite)->m_children.size()>0)
             {
 
-//                if (ImGui::TreeNode((*ite)->GetGameObject()->name.c_str()))
-//                {
-//                    showTransform((*ite)->GetGameObject()->transform);
-//                    ImGui::TreePop();
-//                }
-                ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
-                if (ImGui::TreeNodeEx((void*)(intptr_t)9, node_flags, (*ite)->GetGameObject()->name.c_str()))
+                if (ImGui::TreeNode((*ite)->GetGameObject()->name.c_str()))
                 {
                     if (ImGui::IsItemClicked())
                     {
-                    Debug::Log("选中---父级对象 %s \n",(*ite)->GetGameObject()->name.c_str());
+                    Debug::Log("选中--1-父级对象 %s \n",(*ite)->GetGameObject()->name.c_str());
                     }
                     showTransform((*ite)->GetGameObject()->transform);
                     ImGui::TreePop();
+                }else
+                {
+                    if (ImGui::IsItemClicked())
+                    {
+                        Debug::Log("选中--2-父级对象 %s \n",(*ite)->GetGameObject()->name.c_str());
+                    }
                 }
+//                ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
+//                if (ImGui::TreeNodeEx((void*)(intptr_t)9, node_flags, (*ite)->GetGameObject()->name.c_str()))
+//                {
+//                    if (ImGui::IsItemClicked())
+//                    {
+//                        Debug::Log("选中---父级对象 %s \n",(*ite)->GetGameObject()->name.c_str());
+//                    }
+//                    showTransform((*ite)->GetGameObject()->transform);
+//                    ImGui::TreePop();
+//                }
                 
             }else
             {
