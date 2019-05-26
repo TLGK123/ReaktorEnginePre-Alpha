a91162a7d65dfaa90be4016f827d2ebfaa53a048
blue <1075112523@qq.com>
2019-05-27 00:59:03 +0800

    重新整理设计相机

--- include/im.h
+++ include/im.h
@@ -14,7 +14,7 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "learnopengl/shader_m.h"
-#include "learnopengl/camera.h"
+//#include "learnopengl/camera.h"
 #include "learnopengl/model.h"
 #include <iostream>
 
