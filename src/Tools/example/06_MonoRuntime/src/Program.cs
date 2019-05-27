d40c0c3d424d59807b7ded340c372e58c798b665
WangYiMing <1075112523@qq.com>
2019-05-27 16:18:56 +0800

    添加了mono结合c++的例子

---
+++ src/Tools/example/06_MonoRuntime/src/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoCsharp
+{
+    public static class MainTest
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("你好,世界");
+            System.Console.Read();
+        }
+    }
+}
---
+++ src/Tools/example/06_MonoRuntime/src/main.cpp
@@ -0,0 +1,89 @@
+
+#include <iostream>
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/class.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/mono-config.h>
+#include <exception>
+
+#if !__APPLE__
+#include <stdio.h>
+#include <direct.h>
+#else
+#include "iostream"
+#include <unistd.h>
+#endif
+
+//#include <direct.h>
+
+std::string getPath(std::string assetpath);
+
+
+int main()
+{
+    std::cout << "Hello World! in c++  \n";
+    // 打印当前路径
+
+    MonoDomain *domain;
+
+    auto s = getPath("Library/shared/TestDll.dll");
+    const char *csharp_dll = s.c_str();
+    try
+    {
+        // 初始化JIT
+        domain = mono_jit_init("TestDll");
+        // 加载程序集
+        MonoAssembly *assembly = mono_domain_assembly_open(domain, csharp_dll);
+        MonoImage *image = mono_assembly_get_image(assembly);
+        // 获取MonoClass
+        MonoClass *mono_class = mono_class_from_name(image, "TestDll", "Main");
+        // 获取方法
+        //MonoMethodDesc *method_desc = mono_method_desc_new("csharp.BindingTest:Main()", true);
+        MonoMethodDesc *method_desc = mono_method_desc_new("TestDll.Main:SayHello()", true);
+        MonoMethod *method = mono_method_desc_search_in_class(method_desc, mono_class);
+        mono_method_desc_free(method_desc);
+        // 调用方法
+        mono_runtime_invoke(method, nullptr, nullptr, nullptr);
+        // 释放JIT
+        mono_jit_cleanup(domain);
+    }
+    catch (std::exception* e)
+    {
+        std::cout << e->what();
+    }
+}
+
+extern "C"
+{
+    void DoSomething ();
+}
+
+void DoSomething ()
+{
+    std::cout << " some function define in c++ ! "<<std::endl;
+}
+#define MAX_PATH 250
+
+std::string getPath(std::string assetpath)
+{
+    char buffer[MAX_PATH];
+    getcwd(buffer, MAX_PATH);
+    std::string path = buffer;
+    std::string name = "TmingEngine";
+    int cutIndex = path.find(name);
+    
+    std::string  temp = path.substr(0, cutIndex + name.length());
+    if (assetpath.length()>0)
+    {
+        path = temp + "/" + assetpath;
+    }
+    else
+    {
+        path = temp;
+    }
+    
+    // std::cout<<"当前路径" <<path<<std::endl;
+    // std::cout <<"AssetPath: "+ path << std::endl;
+    return path;
+}
