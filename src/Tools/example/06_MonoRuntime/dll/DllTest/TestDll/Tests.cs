15bd1feb85dfc78af84c4c8205c05d92e49c99ec
blue <1075112523@qq.com>
2019-05-28 08:36:34 +0800

    添加DLL的原始工程

---
+++ src/Tools/example/06_MonoRuntime/dll/DllTest/TestDll/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TestDll
+{
+    public  class Main
+    {
+        
+        public static void SayHello()
+        {
+           string s = "おはよう";
+           Console.WriteLine(s);
+           DoSomething();
+           DoSomething2("我也很无奈啊");
+        }
+
+
+        [DllImport ("__Internal", EntryPoint="DoSomething")]
+        static extern void DoSomething();
+        
+        [DllImport ("__Internal", EntryPoint="DoSomething2")]
+        static extern void DoSomething2(string message);
+    }
+}
