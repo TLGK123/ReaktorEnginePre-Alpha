4eb2be5c78e3f64ea6f7d6e6a3656d45a08b1649
WangYiMing <1075112523@qq.com>
2019-05-29 22:13:55 +0800

    学习c#多线程

--- src/Tools/example/06_MonoRuntime/dll/DllTest/TestDll/Tests.cs
+++ src/Tools/example/06_MonoRuntime/dll/DllTest/TestDll/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 
 namespace TestDll
 {
@@ -13,10 +14,30 @@ namespace TestDll
            Console.WriteLine(s);
            DoSomething();
            DoSomething2("我也很无奈啊");
+<<<<<<< Updated upstream
         //   DoSomething3("这段数据来自c#");
            
            DoSomething4((f)=>Console.WriteLine("C# Print c++ data :"+ f));
            
+=======
+           
+           Console.WriteLine("Main treard start ---");
+           
+           Main m = new Main();
+           
+           
+           Thread t = new Thread(m.PrintNanme);
+           t.Start(" wang wu");
+
+           for (int i = 0; i < 10; i++)
+           {
+                Console.WriteLine("main Tread num :  " + i);
+                Thread.Sleep(100);
+           }
+           Console.WriteLine("Main thread: Call Join(), to wait until ThreadProc ends.");
+           t.Join();
+           Console.WriteLine("Main thread: ThreadProc.Join has returned. ");
+>>>>>>> Stashed changes
         }
 
         public delegate void  CallFromNative(string cshapdata);
@@ -25,6 +46,7 @@ namespace TestDll
         static extern void DoSomething();
         
         [DllImport ("__Internal", EntryPoint="DoSomething2")]
+<<<<<<< Updated upstream
         static extern void DoSomething2(string message);  
         
         [DllImport ("__Internal", EntryPoint="DoSomething3")]
@@ -34,5 +56,40 @@ namespace TestDll
         static extern void DoSomething4(CallFromNative d);
        
         
+=======
+        static extern void DoSomething2(string message);
+
+
+        public static void PrintNum()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("subTread num :  " + i);
+                Thread.Sleep(100);
+            }
+            
+        }
+        
+        public static void PrintNumInTimes(object times )
+        {
+            for (int i = 0; i < (int)times; i++)
+            {
+                Console.WriteLine( "total : "+ times +  " subTread num :  " + i);
+                Thread.Sleep(100);
+            }
+            
+        }
+        
+        public void PrintNanme(object names )
+        {
+            for (int i = 0; i < (int)5; i++)
+            {
+                Console.WriteLine( " subTread name :  " + names);
+                Thread.Sleep(100);
+            }
+            
+        }
+
+>>>>>>> Stashed changes
     }
 }
