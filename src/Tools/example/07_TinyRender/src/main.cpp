9e64f4d9b2bab330cb0508dc5398fe7ea0bd510c
WangYiMing <1075112523@qq.com>
2019-05-31 21:33:44 +0800

    画三角形

--- src/Tools/example/07_TinyRender/src/main.cpp
+++ src/Tools/example/07_TinyRender/src/main.cpp
@@ -5,20 +5,46 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+struct Vector2
+{
+    int x;
+    int y;
+public:
+    Vector2(int _x, int _y )
+    {
+        x = _x;
+        y = _y;
+    }
     
-    for (int x = x0;x <= x1 ; x+=1) {
-        int  y   = (int)((y1 - y0 ) * 1.0 / (x1 - x0) * (x - x0 )) +  y0;
+};
+
+void line(Vector2 p1, Vector2 p2, TGAImage &image, TGAColor color) {
+    if(p2.x < p1.x)
+    {
+        Vector2 temp = p2;
+        p2 =p1;
+        p1= temp;
+    }
+    
+    for (int x = p1.x ; x <= p2.x ; x+= 1 )
+    {
+        int  y = (int)((p2.y - p1.y ) * 1.0 / (p2.x - p1.x) * (x - p1.x)) +  p1.y;
         image.set(x, y, color);
     }
 }
 
+void triangle(Vector2 p1, Vector2 p2 , Vector2 p3 ,TGAImage &image, TGAColor color)
+{
+    line(p1,p2,image,color);
+    line(p2,p3,image,color);
+    line(p3,p1,image,color);
+}
+
+
 int main(int argc, char** argv) {
 	TGAImage image(1000, 1000, TGAImage::RGB);
-//	image.set(52, 41, red);
-    
-    line(0,0,800,500, image,red);
+   // line( Vector2(1000,0),Vector2(0,1000), image, red);
+    triangle( Vector2(20,20),Vector2(800,500),Vector2(300,900), image,red);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
