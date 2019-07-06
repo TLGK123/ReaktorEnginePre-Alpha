aea4b41049a9ef439fb9573795460e4d6c56bc6c
blue <1075112523@qq.com>
2019-07-06 10:38:35 +0800

    log 调整 以及screen 初始化优化

--- src/Editor/Core/Screen.h
+++ src/Editor/Core/Screen.h
@@ -50,7 +50,6 @@ public:
 	bool show_demo_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	Context * screenContext;
 	int CurrentFrameTextureID = 0;
     int CurrentFrameTextureIDCa = 0;
     
