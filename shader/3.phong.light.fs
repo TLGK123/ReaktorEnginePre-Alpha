caed0596e64e83180cd7ed0ff105784fbe7791f1
blue <1075112523@qq.com>
2019-07-15 23:41:34 +0800

    漫反射贴图

--- shader/3.phong.light.fs
+++ shader/3.phong.light.fs
@@ -6,14 +6,16 @@ uniform vec3 viewPos;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;        //纹理坐标
 
 struct Material
 {
-    vec3 ambient;
-    vec3 diffuse;
+    //vec3 ambient;      //大多数情况下和漫反射颜色一致
+    sampler2D diffuse;   //使用漫反射贴图代替物体漫反射颜色
     vec3 specular;
     float shininess;
 };
+
 
 struct Light
 {
@@ -29,12 +31,11 @@ uniform Light light;
 
 void main()
 {
-    vec3 ambient = light.ambient * material.ambient;
-    
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
     vec3 lightDir = normalize(light.position - FragPos);
     vec3 normal = normalize(Normal);
     float diff = max(dot(normal , lightDir),0);
-    vec3 diffuse = light.diffuse * (material.diffuse * diff);
+    vec3 diffuse = light.diffuse * vec3(texture(material.diffuse,TexCoords)) * diff;
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
