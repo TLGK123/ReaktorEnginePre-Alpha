b59e47df01544cbaa92ec685a5a79330ab7bea39
WangYiMing <1075112523@qq.com>
2019-07-16 13:05:04 +0800

    点光源+光强衰弱

--- shader/3.phong.light.fs
+++ shader/3.phong.light.fs
@@ -19,12 +19,16 @@ struct Material
 
 struct Light
 {
-    //vec3 position;     // 使用平行光（定向光）模式。位置信息就不重要
-    vec3 direction;      //光照计算需求一个从片段至光源的光线方向，人们更习惯定义定向光为一个从光源出发的全局方向
+    vec3 position;     // 点光源的 位置信息
+    //vec3 direction;   //平行光的方向 光照计算需求一个从片段至光源的光线方向，人们更习惯定义定向光为一个从光源出发的全局方向
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;  //光强的衰弱系数 就是利用 关于距离的一元二次方程的 倒数  1.0/（ a* d^2 + b*d +c）
+    float linear;    // 线性系数
+    float quadratic; // 平方系统
 
 };
 
@@ -33,9 +37,13 @@ uniform Light light;
 
 void main()
 {
+    float distance = length(light.position - FragPos); //像素点距离光源距离，下面是衰弱计算
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+
     vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
-    //vec3 lightDir = normalize(light.position - FragPos);
-    vec3 lightDir = normalize(-light.direction);
+    vec3 lightDir = normalize(light.position - FragPos); //点光源点方向计算
+    //vec3 lightDir = normalize(-light.direction);   //平行光点方向
     vec3 normal = normalize(Normal);
     float diff = max(dot(normal , lightDir),0);
     vec3 diffuse = light.diffuse * vec3(texture(material.diffuse,TexCoords)) * diff;
@@ -44,6 +52,10 @@ void main()
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir,reflectDir),0),material.shininess);
     vec3 specular = light.specular * (spec * vec3(texture(material.specular,TexCoords)));
+
+    ambient  *= attenuation; 
+    diffuse  *= attenuation;
+    specular *= attenuation;
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result , 1.0);
--- src/Editor/Core/Screen.cpp
+++ src/Editor/Core/Screen.cpp
@@ -377,9 +377,9 @@ void Screen::Render_SceneObjectForEditorCamera()
         model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
         lightingShader.use();
-       // lightingShader.setVec3("light.position", lightPos);
+        lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", EditorCamera.Position);
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+      //  lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
 //        lightColor.x = sin(glfwGetTime() * 2.0f);
 //        lightColor.y = sin(glfwGetTime() * 0.7f);
 //        lightColor.z = sin(glfwGetTime() * 1.3f);
@@ -390,6 +390,9 @@ void Screen::Render_SceneObjectForEditorCamera()
         lightingShader.setVec3("light.diffuse", diffuseColor);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         
+        lightingShader.setFloat("light.constant",  1.0f);
+        lightingShader.setFloat("light.linear",    0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
         
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
