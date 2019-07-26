b9cdf9e61d2875bae1c596e34d43fdcd244abb38
WangYiMing <1075112523@qq.com>
2019-07-26 18:06:02 +0800

    自己写二维向量

--- src/Engine/Math/Vector2.cpp
+++ src/Engine/Math/Vector2.cpp
@@ -1,20 +1,20 @@
 #include "Vector2.h"
 #include <stdio.h>
-//==================
-
-//= NAMESPACES =====
+////==================
+//
+////= NAMESPACES =====
 using namespace std;
-//==================
-
-namespace TmingEngine::Math
+////==================
+//
+namespace TmingEngine
 {
-	const Vector2 Vector2::Zero(0.0f, 0.0f);
-	const Vector2 Vector2::One(1.0f, 1.0f);
+    const Vector2 Vector2::Zero(0.0f, 0.0f);
+    const Vector2 Vector2::One(1.0f, 1.0f);
 
-	string Vector2::ToString() const
-	{
-		char tempBuffer[200];
-		sprintf(tempBuffer, "X:%f, Y:%f", x, y);
-		return string(tempBuffer);
-	}
+    string Vector2::ToString() const
+    {
+        char tempBuffer[200];
+        sprintf(tempBuffer, "X:%f, Y:%f", x, y);
+        return string(tempBuffer);
+    }
 }
