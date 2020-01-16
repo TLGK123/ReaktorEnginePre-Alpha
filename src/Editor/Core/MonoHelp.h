73bdfb008a7205590bd55934da5a01d6e2eb75ac
lillteblue <1075112523@qq.com>
2020-01-16 22:25:24 +0800

    添加mono

---
+++ src/Editor/Core/MonoHelp.h
@@ -0,0 +1,62 @@
+//The MIT License
+//
+//Copyright(c) 2016 - 2019 littleblue
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this softwareand associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//copies of the Software, and to permit persons to whom the Software is furnished
+//to do so, subject to the following conditions :
+//
+//The above copyright noticeand this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//  Selection.hpp
+//  Editor
+//
+//  Created by lillteblue on 2019/11/5.
+//
+#pragma once
+
+#include <iostream>
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/class.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/mono-config.h>
+#include <exception>
+
+#if !__APPLE__
+#include <stdio.h>
+#include <direct.h>
+#else
+#include "iostream"
+#include <unistd.h>
+#endif
+
+#include<string>
+
+#if _MSC_VER
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API
+#endif // _MSC_VER
+
+namespace TmingEngine
+{
+	class MonoHelp
+	{
+	public:
+		int Run(std::string path);
+	};
+
+}
+
