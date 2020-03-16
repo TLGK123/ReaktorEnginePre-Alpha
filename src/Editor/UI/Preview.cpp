2abd4ef7a120239fa21386999c4a620cb7bff557
wangyiming <1075112523@qq.com>
2020-03-16 20:29:07 +0800

    SceneView Size be full by the windows

--- src/Editor/UI/Preview.cpp
+++ src/Editor/UI/Preview.cpp
@@ -43,8 +43,8 @@ void TmingEngine::Preview::Update()
         return;
     }
     ImTextureID my_tex_id = (void *)ImageId;
-    float my_tex_w = ImGui::GetWindowSize().x-35;
-    float my_tex_h = ImGui::GetWindowSize().y-35;
+    float my_tex_w = ImGui::GetWindowSize().x - 20;
+    float my_tex_h = ImGui::GetWindowSize().y - 50;
     //----------------------------------------------------------------- -1 -1  Image reversal
 
 	ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), ImVec2(0, 0), ImVec2(-1, -1), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));	
