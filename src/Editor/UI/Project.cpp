ed1f5d884f63904e648e3ed4116db88c504fa701
wangyiming <1075112523@qq.com>
2020-03-17 18:00:29 +0800

    add file menu

--- src/Editor/UI/Project.cpp
+++ src/Editor/UI/Project.cpp
@@ -25,6 +25,7 @@
 void AssetTree(string path);
 void AssetTreeL(string path);
 void AssetTreeR(string path);
+void MenuFile();
 
 TmingEngine::Project::~Project()
 {
@@ -236,7 +237,16 @@ void AssetTree(string path)
 
 					}
 
+						ImGui::OpenPopup("my_file_popup");
+
 				}
+
+				if (ImGui::BeginPopup("my_file_popup"))
+				{
+					MenuFile();
+					ImGui::EndPopup();
+				}
+
 			}
 		}
 	}
@@ -246,6 +256,71 @@ void AssetTree(string path)
 		auto res = getPathFileOrFolderinfo(path);
 		pathCache.insert(pair<string, vector<PathObjInfo>>(path, res));
 	}
+}
+
+void MenuFile()
+{
+	ImGui::MenuItem("(dummy menu)", NULL, false, false);
+	if (ImGui::MenuItem("New")) {}
+	if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+	if (ImGui::BeginMenu("Open Recent"))
+	{
+		ImGui::MenuItem("fish_hat.c");
+		ImGui::MenuItem("fish_hat.inl");
+		ImGui::MenuItem("fish_hat.h");
+		if (ImGui::BeginMenu("More.."))
+		{
+			ImGui::MenuItem("Hello");
+			ImGui::MenuItem("Sailor");
+			if (ImGui::BeginMenu("Recurse.."))
+			{
+				MenuFile();
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenu();
+	}
+	if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+	if (ImGui::MenuItem("Save As..")) {}
+	ImGui::Separator();
+	if (ImGui::BeginMenu("Options"))
+	{
+		static bool enabled = true;
+		ImGui::MenuItem("Enabled", "", &enabled);
+		ImGui::BeginChild("child", ImVec2(0, 60), true);
+		for (int i = 0; i < 10; i++)
+			ImGui::Text("Scrolling Text %d", i);
+		ImGui::EndChild();
+		static float f = 0.5f;
+		static int n = 0;
+		static bool b = true;
+		ImGui::SliderFloat("Value", &f, 0.0f, 1.0f);
+		ImGui::InputFloat("Input", &f, 0.1f);
+		ImGui::Combo("Combo", &n, "Yes\0No\0Maybe\0\0");
+		ImGui::Checkbox("Check", &b);
+		ImGui::EndMenu();
+	}
+	if (ImGui::BeginMenu("Colors"))
+	{
+		float sz = ImGui::GetTextLineHeight();
+		for (int i = 0; i < ImGuiCol_COUNT; i++)
+		{
+			const char* name = ImGui::GetStyleColorName((ImGuiCol)i);
+			ImVec2 p = ImGui::GetCursorScreenPos();
+			ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + sz, p.y + sz), ImGui::GetColorU32((ImGuiCol)i));
+			ImGui::Dummy(ImVec2(sz, sz));
+			ImGui::SameLine();
+			ImGui::MenuItem(name);
+		}
+		ImGui::EndMenu();
+	}
+	if (ImGui::BeginMenu("Disabled", false)) // Disabled
+	{
+		IM_ASSERT(0);
+	}
+	if (ImGui::MenuItem("Checked", NULL, true)) {}
+	if (ImGui::MenuItem("Quit", "Alt+F4")) {}
 }
 
 void AssetTreeL(string path)
