b8bac7e72d97433489c8efa5cf36ed96b3b24ce0
lillteblue <1075112523@qq.com>
2020-05-18 21:41:11 +0800

    修改context 把初始化的内容从默认构造函数中移除,把销毁对象要的处理从默认析构函数中处理

--- src/Engine/Core/Context.cpp
+++ src/Engine/Core/Context.cpp
@@ -31,14 +31,11 @@ namespace TmingEngine
 	{
 	}
 
-	Context::~Context()
+
+	void Context::OnDestory()
 	{
 		for (auto i = m_subsystems.size() - 1; i > 0; i--)
 			delete m_subsystems[i];
-
-		// Index 0 is the actual Engine instance, which is the instance
-		// that called this deconstructor in the first place. A deletion
-		// will result in a crash.
 	}
 
 	void Context::RegisterSubsystem(Subsystem* subsystem)
