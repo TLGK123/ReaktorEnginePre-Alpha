596160d2cc5274304d6ad01f26fc414277748ec5
lillteblue <1075112523@qq.com>
2020-06-16 13:13:42 +0800

    使用重心坐标进行光栅化

--- src/Engine/Core/Math/Vector3.h
+++ src/Engine/Core/Math/Vector3.h
@@ -67,6 +67,11 @@ namespace TmingEngine
 			return Vector3(x + other.x, y + other.y, z + other.z);
 		}
 
+		Vector3 operator +( int  offset ) const
+		{
+			return Vector3(x + offset, y + offset, z + offset);
+		}
+
 		Vector3 operator -(const Vector3& other) const
 		{
 			return Vector3(x - other.x, y - other.y, z - other.z);
