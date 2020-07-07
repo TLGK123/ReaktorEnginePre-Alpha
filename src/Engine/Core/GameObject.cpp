499bad00f9c90d50363b551927f632bef2e7f672
lillteblue <1075112523@qq.com>
2020-07-08 01:30:01 +0800

    change license

--- src/Engine/Core/GameObject.cpp
+++ src/Engine/Core/GameObject.cpp
@@ -1,60 +1,52 @@
-//The MIT License
-//
-//Copyright(c) 2016 - 2020 littleblue
-//
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this softwareand associated documentation files(the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-//copies of the Software, and to permit persons to whom the Software is furnished
-//to do so, subject to the following conditions :
-//
-//The above copyright noticeand this permission notice shall be included in
-//all copies or substantial portions of the Software.
-//
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-//FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
-//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-//IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*
+			This file is part of:
+				TmingEngine
+			https://github.com/xiaomingfun/TmingEngine
 
-//
-//  GameObject.cpp
-//  01_CaculateCode
-//
-//  Created by lillteblue = on 2019/5/20.
-//
+   Copyright 2018 - 2020 TmingEngine
 
-//= INCLUDES ==========
+   File creator: littleblue
+
+   TmingEngine is open-source software and you can redistribute it and/or modify
+   it under the terms of the License; besides, anyone who use this file/software must include this copyright announcement.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #include "GameObject.hpp"
 #include "Context.h"
-//====================
 
- namespace TmingEngine
+namespace TmingEngine
 {
-    GameObject::GameObject()
-    {
-        Initializer();
-    }
-    
-    GameObject::GameObject(string ObjectName)
-    {
-        name = ObjectName;
-        Initializer();
-    }
-    
-    void GameObject::Initializer()
-    {
-        auto context = & Global<Context>();
-        transform = new Transform( context,this,nullptr);
-    }
-    
-    string GameObject::GetGameObjectName()
-    { 
-        return name;
-    }
+	GameObject::GameObject()
+	{
+		Initializer();
+	}
 
-    
+	GameObject::GameObject(string ObjectName)
+	{
+		name = ObjectName;
+		Initializer();
+	}
+
+	void GameObject::Initializer()
+	{
+		auto context = &Global<Context>();
+		transform = new Transform(context, this, nullptr);
+	}
+
+	string GameObject::GetGameObjectName()
+	{
+		return name;
+	}
 }
-
