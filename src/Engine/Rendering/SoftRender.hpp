c94481350c09463d69b6b8876c5f96bca6a912fe
lillteblue <1075112523@qq.com>
2020-07-10 00:48:41 +0800

    abstract SoftGL.hpp

--- src/Engine/Rendering/SoftRender.hpp
+++ src/Engine/Rendering/SoftRender.hpp
@@ -31,6 +31,8 @@
 #include "Rendering/Model.hpp"
 #include "Rendering/Primitive.hpp"
 #include "Rendering/GouraudShader.hpp"
+#include "Rendering/Color.hpp"
+#include "Rendering/Light.hpp"
 
 namespace TmingEngine
 {
@@ -39,6 +41,11 @@ namespace TmingEngine
 	public:
 		SoftRender() {};
 		~SoftRender() {};
+
+		unsigned int frameID;
+		int frameWidth;
+		int frameHeigh;
+		Light sunlitght;
 
 		//---------- Simulate  VRAM    Data
 		vector<Primitive> primitiveDatas;
@@ -85,6 +92,132 @@ namespace TmingEngine
 		//
 		void GenerateFrames()
 		{
+			glGenTextures(1, &frameID);
+
+			glBindTexture(GL_TEXTURE_2D, frameID);
+			// set the texture wrapping parameters
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			// set texture wrapping to GL_REPEAT (default wrapping method)
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			// set texture filtering parameters
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			//// load image, create texture and generate mipmaps
+			int width, height, nrChannels;
+
+			TGAImage image(frameWidth, frameHeigh, TGAImage::RGB);
+
+			Vector2 t0[3] = { Vector2(40, 40),   Vector2(250, 300) , Vector2(350, 100) };
+			Vector2 t1[3] = { Vector2(380, 50),  Vector2(450, 10),   Vector2(370, 180) };
+			Vector2 t2[3] = { Vector2(180, 350), Vector2(120, 260), Vector2(130, 400) };
+			///-----step 1
+			/*
+			triangle(t0[0], t0[1], t0[2], image, red);
+			fillTriangleFromEdge(t0[0], t0[1], t0[2], image, red);
+
+			triangle(t1[0], t1[1], t1[2], image, blue);
+			fillTriangleLinerScan(t1[0], t1[1], t1[2], image, blue);
+
+			triangle(t2[0], t2[1], t2[2], image, green);
+			fillTriangleLinerScan(t2[0], t2[1], t2[2], image, green);
+			*/
+
+			//-----step 2
+
+			sunlitght.Direction = Vector3(0, 1, -1);
+			sunlitght.Color = Color(0.5, 0.5, 0);
+
+			Vector3 CameraPos = Vector3(0, 1, 3);
+			Vector3 center(0, 0, 0);	//相机朝向原点
+			Vector3 up(0, 1, 0);		//相机向上
+
+			Matrix model(4, 4, {
+				1,0,0,0,
+				0,1,0,0,
+				0,0,1,0,
+				0,0,0,1,
+				});
+
+			Matrix view = LookAt(CameraPos, center, up);
+
+			Matrix perspective = Perspective(1, 1, 1, 3);
+
+			Matrix orthographic = Orthographic(1.5, 1.5, 0.5, 10);
+
+			Matrix viewPoint = Viewport(0, 0, gameWidth, gameHeight);
+
+			shader->SetModel(model);
+			shader->SetView(view);
+			shader->SetProjection(perspective);
+			shader->SetViewPoint(viewPoint);
+
+			for (int i = 0; i < testCharacter.meshes[0].indices.size(); i += 3)
+			{
+				int  index1 = testCharacter.meshes[0].indices[i];
+				int  index2 = testCharacter.meshes[0].indices[i + 1];
+				int  index3 = testCharacter.meshes[0].indices[i + 2];
+
+				Vertex v1 = (testCharacter.meshes[0].vertices[index1]);
+				Vertex v2 = (testCharacter.meshes[0].vertices[index2]);
+				Vertex v3 = (testCharacter.meshes[0].vertices[index3]);
+
+				int len = gameWidth * gameHeight;
+				int* zbuffer = new int[len];
+
+				for (int inedx = 0; inedx < len; inedx++)
+				{
+					zbuffer[inedx] = 10000000;
+				}
+
+				v1.Position = shader->Vertex(v1.Position);
+				v2.Position = shader->Vertex(v2.Position);
+				v3.Position = shader->Vertex(v3.Position);
+
+				fillTriangleFromEdgeWitchZbuffer(
+					v1, v2, v3,
+					image, red, zbuffer);
+			}
+
+			image.flip_horizontally();
+
+			unsigned char* data = image.buffer();
+			width = image.get_width();
+			height = image.get_height();
+			nrChannels = image.get_bytespp();
+
+			if (data)
+			{
+				try
+				{
+					if (nrChannels == 4)
+					{
+						glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+					}
+					else if (nrChannels == 3)
+					{
+						glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+					}
+					else if (nrChannels == 1)
+					{
+						glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+					}
+					else
+					{
+						int c = nrChannels;
+					}
+
+					glGenerateMipmap(GL_TEXTURE_2D);
+				}
+				catch (const std::exception&)
+				{
+				}
+			}
+			else
+			{
+				std::cout << "Failed to load texture" << std::endl;
+			}
+
+			image.clear();
 		}
 	};
 } //namespace TmingEngine
