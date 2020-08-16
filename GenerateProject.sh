0fc1e4d6903f5d0881a95f5f0181faad55db4b68
blue <1075112523@qq.com>
2020-08-17 01:11:31 +0800

    fix for make

--- GenerateProject.sh
+++ GenerateProject.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 if [ ! -d "build" ]; then
   mkdir build
 fi
@@ -7,16 +8,20 @@ cmake -H../ -B.		#cmake -Hpath/to/source -Bpath/to/build
 sys=`uname  -a`
 sysname=$(echo $sys | sed 's/ //g' | tr '[A-Z]' '[a-z]')
 
-if [[ ! -z $(echo $sysname | grep "darwin")]];then
+if [ ! -z $(echo $sysname | grep "darwin") ]
+then
     echo "Mac"
 	make -j4
-elif [[ ! -z $(echo $sysname | grep "linux") ]];then
+elif [ ! -z $(echo $sysname | grep "linux") ]
+then
     echo "Linux"
 	make -j4
-elif [[ ! -z $(echo $sysname | grep "ubuntu")  ]];then	
+elif [ ! -z $(echo $sysname | grep "ubuntu") ]
+then	
     echo "Ubuntu"
 	make -j4
-elif [[ ! -z $(echo $sysname | grep "windows")  ]];then
+elif [ ! -z $(echo $sysname | grep "windows") ]
+then
 	echo "------------------------------------------------------------------------"
 	echo "--------------------Successful to generate vs project-------------------"
     echo "Please use Visual Studio to open build/TmingEngine.sln "
