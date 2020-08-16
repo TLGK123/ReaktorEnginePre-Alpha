6e1b24c4d4e7d2df1700486536961fcf4741f986
lillteblue <1075112523@qq.com>
2020-08-16 23:40:05 +0800

    optimize the windows build

--- GenerateProject.sh
+++ GenerateProject.sh
@@ -2,6 +2,29 @@ if [ ! -d "build" ]; then
   mkdir build
 fi
 cd build
-cmake -H../ -B.
-make -j4
-#cmake -Hpath/to/source -Bpath/to/build
+cmake -H../ -B.		#cmake -Hpath/to/source -Bpath/to/build
+
+sys=`uname  -a`
+sysname=$(echo $sys | sed 's/ //g' | tr '[A-Z]' '[a-z]')
+
+if [[ ! -z $(echo $sysname | grep "darwin")]];then
+    echo "Mac"
+	make -j4
+elif [[ ! -z $(echo $sysname | grep "linux") ]];then
+    echo "Linux"
+	make -j4
+elif [[ ! -z $(echo $sysname | grep "ubuntu")  ]];then	
+    echo "Ubuntu"
+	make -j4
+elif [[ ! -z $(echo $sysname | grep "windows")  ]];then
+	echo "------------------------------------------------------------------------"
+	echo "--------------------Successful to generate vs project-------------------"
+    echo "Please use Visual Studio to open build/TmingEngine.sln "
+	echo "Then set the Editor as a startup project                                "
+	echo "build and run   ,       engoy it !!!                                    "
+	echo "------------------------------------------------------------------------"
+else
+    echo $sysname
+fi
+
+
