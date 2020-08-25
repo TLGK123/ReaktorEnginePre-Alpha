d74922777658dd09c5b6910cca6fe6cfffcae479
lillteblue <1075112523@qq.com>
2020-08-25 08:45:11 +0800

    add c# code

---
+++ src/Managed/TmingCore/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmingCore
+{
+    public class Main
+    {
+        public static void Start()
+        {
+            Console.WriteLine("TmingEngine core start success");
+            Console.WriteLine("TmingEngine core start do something else");
+            int a = Add(1, 6);
+        }
+
+        public static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
