aa22d7193c6b880c1b0d3fa3d887e707a4520cdd
lillteblue <1075112523@qq.com>
2020-08-30 11:36:52 +0800

    diff plantform

--- src/Engine/Plantform/FileSystem/FileSystem.h
+++ src/Engine/Plantform/FileSystem/FileSystem.h
@@ -1,149 +1,79 @@
 /*
-Copyright(c) 2016-2019 Panos Karabelas
+			This file is part of:
+				TmingEngine
+			https://github.com/xiaomingfun/TmingEngine
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-copies of the Software, and to permit persons to whom the Software is furnished
-to do so, subject to the following conditions :
+   Copyright 2018 - 2020 TmingEngine
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+   File creator: littleblue
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   TmingEngine is open-source software and you can redistribute it and/or modify
+   it under the terms of the License; besides, anyone who use this file/software must include this copyright announcement.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
-#pragma once
-
-//= INCLUDES ==================
 #include <vector>
 #include <string>
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include <sstream>
+
 #include "Core/EngineDefs.h"
 #include "Core/Log.h"
-#include<iostream>
 
-#if !__APPLE__
+#ifdef _WIN32
+//define something for Windows (32-bit and 64-bit, this part is common)
 #include <stdio.h>
 #include <direct.h>
+#ifdef _WIN64
+   //define something for Windows (64-bit only)
 #else
-#include "iostream"
+   //define something for Windows (32-bit only)
+#endif
+#elif __APPLE__
+
+#if TARGET_IPHONE_SIMULATOR
+// iOS Simulator
+#elif TARGET_OS_IPHONE
+// iOS device
+#elif TARGET_OS_MAC
+#include <iostream>
 #include <unistd.h>
+#else
+#   error "Unknown Apple platform"
+#endif
+#elif __ANDROID__
+// android
+#elif __linux__
+// linux
+#elif __unix__ // all unices not caught above
+// Unix
+#elif defined(_POSIX_VERSION)
+// POSIX
+#else
+#   error "Unknown compiler"
 #endif
 
 #define MAX_PATH 250
-
-//=============================
-
-//=========================================================
-static const std::string NOT_ASSIGNED = "N/A";
-static const int NOT_ASSIGNED_HASH = -1;
-// Metadata extensions
-static const char* METADATA_EXTENSION = ".xml";
-static const char* METADATA_TYPE_TEXTURE = "Texture";
-static const char* METADATA_TYPE_AUDIOCLIP = "Audio_Clip";
-// Engine file extensions
-static const char* EXTENSION_WORLD = ".world";
-static const char* EXTENSION_MATERIAL = ".mat";
-static const char* EXTENSION_MODEL = ".model";
-static const char* EXTENSION_PREFAB = ".prefab";
-static const char* EXTENSION_SHADER = ".shader";
-static const char* EXTENSION_TEXTURE = ".texture";
-static const char* EXTENSION_MESH = ".mesh";
-//=========================================================
 
 namespace TmingEngine
 {
 	class ENGINE_CLASS FileSystem :public Subsystem
 	{
 	public:
-		bool Initialize();
-		void SaveFile(std::string path, std::string content);
-		std::string GetFileContent(std::string f);
+
 		static std::string getPath(std::string assetpath);
-		//= DIRECTORIES ==============================================
-		static bool CreateDirectory_(const std::string& path);
-		static bool DeleteDirectory(const std::string& directory);
-		static bool DirectoryExists(const std::string& directory);
-		static bool IsDirectory(const std::string& directory);
-		static void OpenDirectoryWindow(const std::string& directory);
-		//============================================================
-
-		//= FILES ============================================================================
-		static bool FileExists(const std::string& filePath);
-		static bool DeleteFile_(const std::string& filePath);
-		static bool CopyFileFromTo(const std::string& source, const std::string& destination);
-		//====================================================================================
-
-		//= DIRECTORY PARSING  =================================================================
-		static std::string GetFileNameFromFilePath(const std::string& path);
-		static std::string GetFileNameNoExtensionFromFilePath(const std::string& filepath);
-		static std::string GetDirectoryFromFilePath(const std::string& filePath);
-		static std::string GetFilePathWithoutExtension(const std::string& filePath);
-		static std::string GetExtensionFromFilePath(const std::string& filePath);
-		static std::string GetRelativeFilePath(const std::string& absoluteFilePath);
-		static std::string GetWorkingDirectory();
-		static std::string GetParentDirectory(const std::string& directory);
-		static std::vector<std::string> GetDirectoriesInDirectory(const std::string& directory);
-		static std::vector<std::string> GetFilesInDirectory(const std::string& directory);
-		//======================================================================================
-
-		//= SUPPORTED FILES IN DIRECTORY ======================================================================
-		static std::vector<std::string> GetSupportedFilesInDirectory(const std::string& directory);
-		static std::vector<std::string> GetSupportedImageFilesFromPaths(const std::vector<std::string>& paths);
-		static std::vector<std::string> GetSupportedAudioFilesFromPaths(const std::vector<std::string>& paths);
-		static std::vector<std::string> GetSupportedScriptFilesFromPaths(const std::vector<std::string>& paths);
-		static std::vector<std::string> GetSupportedModelFilesFromPaths(const std::vector<std::string>& paths);
-		static std::vector<std::string> GetSupportedModelFilesInDirectory(const std::string& directory);
-		static std::vector<std::string> GetSupportedSceneFilesInDirectory(const std::string& directory);
-		//======================================================================================================
-
-		//= SUPPORTED FILE CHECKS =====================================
-		static bool IsSupportedAudioFile(const std::string& path);
-		static bool IsSupportedImageFile(const std::string& path);
-		static bool IsSupportedModelFile(const std::string& path);
-		static bool IsSupportedShaderFile(const std::string& path);
-		static bool IsSupportedFontFile(const std::string& path);
-		static bool IsEngineScriptFile(const std::string& path);
-		static bool IsEnginePrefabFile(const std::string& filePath);
-		static bool IsEngineMaterialFile(const std::string& filePath);
-		static bool IsEngineMeshFile(const std::string& filePath);
-		static bool IsEngineModelFile(const std::string& filePath);
-		static bool IsEngineSceneFile(const std::string& filePath);
-		static bool IsEngineTextureFile(const std::string& filePath);
-		static bool IsEngineShaderFile(const std::string& filePath);
-		static bool IsEngineMetadataFile(const std::string& filePath);
-		//=============================================================
-
-		//= STRING PARSING =============================================================================================================================
-		static std::string GetStringAfterExpression(const std::string& str, const std::string& expression);
-		static std::string GetStringBetweenExpressions(const std::string& str, const std::string& firstExpression, const std::string& secondExpression);
-		static std::string ConvertToUppercase(const std::string& lower);
-		static std::string ReplaceExpression(const std::string& str, const std::string& from, const std::string& to);
-		static std::wstring StringToWstring(const std::string& str);
-		static std::string ResolveIncludeDirectives(const std::string& source, const std::string& directory);
-		//==============================================================================================================================================
-
-		//= SUPPORTED ASSET FILE FORMATS ===============================================================
-		static std::vector<std::string> GetSupportedImageFormats() { return m_supportedImageFormats; }
-		static std::vector<std::string> GetSupportedAudioFormats() { return m_supportedAudioFormats; }
-		static std::vector<std::string> GetSupportedModelFormats() { return m_supportedModelFormats; }
-		static std::vector<std::string> GetSupportedShaderFormats() { return m_supportedShaderFormats; }
-		static std::vector<std::string> GetSupportedScriptFormats() { return m_supportedScriptFormats; }
-		static std::vector<std::string> GetSupportedFontFormats() { return m_supportedFontFormats; }
-		//==============================================================================================
-
-	private:
-		static std::vector<std::string> m_supportedImageFormats;
-		static std::vector<std::string> m_supportedAudioFormats;
-		static std::vector<std::string> m_supportedModelFormats;
-		static std::vector<std::string> m_supportedShaderFormats;
-		static std::vector<std::string> m_supportedScriptFormats;
-		static std::vector<std::string> m_supportedFontFormats;
 	};
 }
